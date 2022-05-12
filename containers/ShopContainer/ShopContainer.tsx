085c52515ad6d3d279705f37217b955414d54034
nguyenanhkhai <kseptember9cs@gmail.com>
2022-05-13 03:52:08 +0700

    fixed: old error

--- containers/ShopContainer/ShopContainer.tsx
+++ containers/ShopContainer/ShopContainer.tsx
@@ -4,6 +4,7 @@ import { ABOUT } from "data/about";
 import { DW_MENS } from "data/dw_men";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { WatchProps } from "types/common";
 
 type ShopContainerProps = {
   className?: string;
@@ -13,11 +14,24 @@ const ShopContainer: React.FC<ShopContainerProps> = (props) => {
   const router = useRouter();
   const brand = router.query.brand as string;
 
+  function getFakeData(brand_: string) {
+    for (let i = 0; i < ABOUT.length; i++) {
+      if (brand_ == ABOUT[i].brand) {
+        return {
+          imgae: ABOUT[i].image,
+          content: ABOUT[i].content,
+        };
+      }
+    }
+  }
+
   return (
     <>
       <Paper className="p-6">
-        <img src={ABOUT[brand].image} alt="" />
-        <p className="mt-4 leading-6 text-justify">{ABOUT[brand].content}</p>
+        <img src={getFakeData(brand)?.imgae} alt="" />
+        <p className="mt-4 leading-6 text-justify">
+          {getFakeData(brand)?.content}
+        </p>
       </Paper>
 
       <div className="flex gap-6 mt-6">
--- data/about.ts
+++ data/about.ts
@@ -1,5 +1,6 @@
-export const ABOUT = {
-  "deniel-wellington": {
+export const ABOUT = [
+  {
+    brand: "deniel-wellington",
     image:
       "//d1rkccsb0jf1bk.cloudfront.net/landingpages/daniel-wellington/xDW_Brand_Room_AW21_Hero_Desktop.jpg.pagespeed.ic.hznF1b3fhY.webp",
     content: `Daniel Wellington watches were originally inspired by a chance meeting
@@ -18,11 +19,12 @@ export const ABOUT = {
     watch up a notch in terms of formality. Browse stunning men's and
     ladies' designs in black, gold, rose, silver and two tone.`,
   },
-  fossil: {
+  {
+    brand: "fossil",
     image:
       "https://d1rkccsb0jf1bk.cloudfront.net/landingpages/fossil/fsl_sp22_ls_ww_spring_essentials_4_spring_essentials_2_watch_shop_hb_ci_1920x700px_en.webp",
     content: `Established in 1984, the Fossil brand is inspired by classic American vintage wear. Fossil watches are designed to make statements, looking to act as a first impression for how wearers are viewed by others. Within the Fossil men's and women's watch collection Swiss craftsmanship is blended seamlessly with timeless designs and contemporary functionality, resulting in a range that has something to define almost anyone. 
           Fossil designers use a wide variety of materials and styles throughout the watch collection, from multi-faceted, colourful metals in gold, silver & rose gold to deconstructed leathers in black, brown tan & blue. Make a statement with technology-laden cases and mechanical watches that beautifully showcase the art of watchmaking, or enjoy a timeless look with minimalistic aesthetics - Fossil provides the discerning watch-buyer with multiple choices. To stay connected, Fossil's range of Fossil Q smartwatches feature fitness tracking, music control and smartphone notifications without compromising style.
           Fossil watches are more than just a practical accessory they are a lifestyle choice that lets the world know who you are at a glance. Browse Fossil's range of men's and women's watches, or experience the same level of quality with different styles - Fossil also makes watches for a variety of other brands including including Diesel, Armani, Adidas and Marc Jacobs.`,
   },
-};
+];
